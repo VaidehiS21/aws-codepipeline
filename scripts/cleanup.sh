57206c93608320aeb09758df41521856e2705929
VaidehiS21 <vaidehi.s@autotroph.tech>
2026-02-16 09:16:37 +0000

    Fix CodeDeploy npm EACCES by chown in AfterInstall

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -2,9 +2,4 @@
 set -e
 
 echo "Cleaning old deployment files..."
-
 sudo rm -rf /home/ubuntu/aws-codepipeline/*
-sudo mkdir -p /home/ubuntu/aws-codepipeline
-
-echo "Fixing permissions..."
-sudo chown -R ubuntu:ubuntu /home/ubuntu/aws-codepipeline
--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
 
+echo "Fixing permissions..."
+sudo chown -R ubuntu:ubuntu /home/ubuntu/aws-codepipeline
+
 echo "Installing dependencies..."
-
 cd /home/ubuntu/aws-codepipeline
-
 npm install --no-fund --no-audit
